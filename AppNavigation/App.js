077ea118725fe2a7071b42b4d6adebc16becfc94
eugiomarques <giovannamarques.contato2@gmail.com>
2024-04-02 03:52:37 -0300

    V1
    Adição da página Registrar e Home.

--- AppNavigation/App.js
+++ AppNavigation/App.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigation } from "@react-navigation/stack";
+import Registrar from './src/pages/Registrar/index'
+import Home from './src/pages/Home/index'
+import {View, Text} from 'react-native'
+
+const Stack = createStackNavigation();
 
 export default function App(){
   return(
+    <><NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen name='Registrar' component={Registrar} />
+        <Stack.Screen name='Home' component={Home} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+    <View>
+        <Text>Login</Text>
+    </View></>
 
   )
 }
